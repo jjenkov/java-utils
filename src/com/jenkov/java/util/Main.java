97efc451c2906d84f411f083b2013912f61cf4e2
Jakob Jenkov <jakob@jenkov.com>
2013-02-15 16:05:16 +0100

    Ready

---
+++ src/com/jenkov/java/util/Main.java
@@ -0,0 +1,12 @@
+package com.jenkov.java.util;
+
+/**
+ * An example Main application in Java, containing the main()
+ * method needed to run the application from the command line.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
